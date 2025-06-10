0f60a86462493d891e2017ed2800aca37fb3b1de
Jaison Villarroel <xyeison0x@gmail.com>
2025-06-10 12:45:48 +0200

    refactor wallet repository

--- src/Wallet/Domain/Repository/WalletRepository.php
+++ src/Wallet/Domain/Repository/WalletRepository.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Src\Wallet\Domain\Repository;
 use Src\Wallet\Domain\ValueObject\WalletId;
 use Src\Wallet\Domain\Entity\Wallet;
+use Src\Client\Domain\ValueObject\ClientId;
 interface WalletRepository
 {
     public function findById(WalletId $id): ?Wallet;
+    public function findByClientId(ClientId $clientId): ?Wallet;
     public function save(Wallet $wallet): void;
     public function update(Wallet $wallet): void;
     public function delete(WalletId $id): void;
