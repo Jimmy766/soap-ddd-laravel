6c2424fd0d5ca2a25ab000ebdb385050f8b4d134
Migo776 <soka.au@gmail.com>
2025-06-11 13:12:29 +0200

    Revert "Implement all needed for Soap"

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,12 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Src\Client\Infrastructure\Repository\EloquentClientRepository;
-use Src\Client\Domain\Repository\ClientRepository;
-use Src\Wallet\Domain\Repository\WalletRepository;
-use Src\Wallet\Infrastructure\Repository\EloquentWalletRepository;
-use Src\Payment\Infrastructure\Repository\EloquentPaymentRepository;
-use Src\Payment\Domain\Repository\PaymentRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,9 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(ClientRepository::class, EloquentClientRepository::class);
-        $this->app->bind(WalletRepository::class, EloquentWalletRepository::class);
-        $this->app->bind(PaymentRepository::class, EloquentPaymentRepository::class);
+        //
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SoapController;
-use App\Http\Controllers\SoapController1;
-use Src\Client\Infrastructure\Controller\CreateClientController;
-use Src\Wallet\Infrastructure\Controller\CheckWalletController;
-use Src\Wallet\Infrastructure\Controller\TopUpController;
-use Src\Payment\Infrastructure\Controller\DoPayController;
-use Src\Payment\Infrastructure\Controller\ConfirmPaymentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::match(['GET', 'POST'],'/soap/client', [CreateClientController::class, 'handle']);
-Route::match(['GET', 'POST'],'/soap/wallet/topup', [TopUpController::class, 'handle']);
-Route::match(['GET', 'POST'],'/soap/wallet/check', [CheckWalletController::class, 'handle']);
-Route::match(['GET', 'POST'],'/soap/payment/dopay', [DoPayController::class, 'handle']);
-Route::match(['GET', 'POST'],'/soap/payment/confirm', [ConfirmPaymentController::class, 'handle']);
